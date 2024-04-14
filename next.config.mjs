c916a2d6c50ae3d765f61b7a18dba7df19566c30
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2024-04-14 11:24:14 -0300

    test no basePath with new subdomain

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/beta",
+  // basePath: "/rcrd-beta",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
