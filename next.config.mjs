c3780e73ea96af13e74f4c656dcf4794b1ff07f1
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2024-04-14 11:16:40 -0300

    change basePath to just /beta

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/rcrd-beta",
+  basePath: "/beta",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
