b525620fef4572e28896b48a8e312ac30119766f
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2024-04-25 10:53:44 -0300

    remove basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: "/rcrd-beta",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
   images: {
