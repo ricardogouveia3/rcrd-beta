9c4f5547671c63e8e81f001d42df108d7b8d9dee
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2024-10-19 10:41:08 -0300

    Footer with current year

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,4 +1,5 @@
 import Card from "../components/Card/Card";
+import { currentYear } from "../utils/date";
 
 type FooterProps = {
   classNames?: string;
@@ -10,7 +11,7 @@ export default function Footer({ classNames = defaultClassNames }: Readonly<Foot
   return (
     <Card classNames={`${classNames}  rounded-lg text-sm text-black dark:text-white text-center`}>
       <p className="text-center font-semibold">
-        2024 - Ricardo Gouveia - <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.pt_BR" target="_blank" className="underline">CC BY-SA 4.0</a><span className="text-xs text-gray-500 font-normal block mt-1">(unless otherwise specified and/or in content replicated from other sources | Logos and trademarks belong to their respective owners)</span>
+        {currentYear} - Ricardo Gouveia - <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.pt_BR" target="_blank" className="underline">CC BY-SA 4.0</a><span className="text-xs text-gray-500 font-normal block mt-1">(unless otherwise specified and/or in content replicated from other sources | Logos and trademarks belong to their respective owners)</span>
       </p>
       <p className="italic text-xs text-gray-600 dark:text-gray-400 mt-3">
         <a className="underline" href="https://github.com/ricardogouveia3/rcrd-beta" target="_blank">Source code</a> - Design inspired by <a className="underline" href="https://www.joshuabrigati.com/" target="_blank">Joshua Brigati</a>
