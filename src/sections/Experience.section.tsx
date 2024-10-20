515a025ce3f8c520d728c3fc9921e7fc6fe00db2
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2024-10-20 17:49:55 -0300

    postpone responsive for experience sections

--- src/sections/Experience.section.tsx
+++ src/sections/Experience.section.tsx
@@ -12,9 +12,9 @@ export default function ExperienceSection() {
   const gridClassNames = `md:col-start-5 lg:col-start-10 md:col-span-2 lg:col-span-3 md:row-start-5 lg:row-start-4`;
 
   return (
-    <Card classNames={`${gridClassNames} flex md:flex-col`}>
+    <Card classNames={`${gridClassNames} flex flex-col`}>
       <h3 className="text-lg/7 mb-4 font-medium text-gray-950 dark:text-white">Work experience:</h3>
-      <ol className="relative border-s border-quartz-900 dark:border-quartz-400 flex md:flex-col items-stretch">
+      <ol className="relative border-s border-quartz-900 dark:border-quartz-400 flex flex-col items-stretch">
         {itemsToRender.map((experience: Experience) => (
           <ExperienceItem
             key={experience.id}
