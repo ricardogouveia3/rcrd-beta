623cc691c4765ef253a35adc317ccf46b7a82208
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2024-10-28 18:51:30 -0300

    masonsy grid

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -10,13 +10,14 @@ export default function ProjectItem({ project }: Readonly<{ project: Project }>)
     <a href={project.link}
       target="_blank"
       rel="noopener noreferrer"
-      className="flex flex-col justify-between overflow-hidden rounded-lg"
-      style={{ backgroundColor: randomColor, color: contrastColor }}
-    >
+      className="flex flex-col justify-between overflow-hidden rounded-lg h-full"
+      style={{ backgroundColor: randomColor, color: contrastColor }}>
+
       <div className="flex flex-col gap-4 p-4 lg:p-6">
         <h3 className="text-lg font-medium">{project.title}</h3>
         <p>{project.description}</p>
       </div>
+
       <div className="flex flex-wrap gap-2 p-4 lg:p-6">
         {project.tags.map((tag: string) => (
           <span key={tag} className="text-sm font-medium">
--- src/sections/Projects.section.tsx
+++ src/sections/Projects.section.tsx
@@ -7,18 +7,72 @@ import { Project } from "../types/Project.type.ts";
 
 export default function ProjectSection() {
   const isAboveMd = useBreakpoint("md").isAboveMd;
-  const maxRendered = isAboveMd ? 6 : 3;
+  const maxRendered = isAboveMd ? projects.length : 3;
   const itemsToRender = projects.slice(0, maxRendered) as Project[];
-  const gridCols = isAboveMd ? "grid-cols-3" : "grid-cols-1";
+  const gridCols = isAboveMd ? "grid-cols-4" : "grid-cols-1";
 
   return (
     <Card classNames={`${gridClassNames.projects}`} contentClassnames="p-4 lg:p-6 flex flex-col gap-4">
       <h3 className="text-lg/7 mb-4 font-medium text-gray-950 dark:text-white">Projects:</h3>
-      <div className={`grid ${gridCols} lg:gap-6 gap-4`}>
-        {itemsToRender.map((project) => (
-          <ProjectItem key={project.id} project={project} />
-        ))}
+
+      <div className={`grid ${gridCols} gap-4`}>
+        {itemsToRender[2] && (
+          <div className="grid gap-4 grid-rows-4">
+            <div className="h-full max-w-full rounded-lg">
+              <ProjectItem key={itemsToRender[0].id} project={itemsToRender[0]} />
+            </div>
+            <div className="h-full max-w-full rounded-lg row-span-2">
+              <ProjectItem key={itemsToRender[1].id} project={itemsToRender[1]} />
+            </div>
+            <div className="h-full max-w-full rounded-lg">
+              <ProjectItem key={itemsToRender[2].id} project={itemsToRender[2]} />
+            </div>
+          </div>
+        )}
+
+        {itemsToRender[5] && (
+          <div className="grid gap-4 grid-rows-4">
+            <div className="h-full max-w-full rounded-lg row-span-2">
+              <ProjectItem key={itemsToRender[3].id} project={itemsToRender[3]} />
+            </div>
+            <div className="h-full max-w-full rounded-lg r">
+              <ProjectItem key={itemsToRender[4].id} project={itemsToRender[4]} />
+            </div>
+            <div className="h-full max-w-full rounded-lg">
+              <ProjectItem key={itemsToRender[5].id} project={itemsToRender[5]} />
+            </div>
+          </div>
+        )}
+
+        {itemsToRender[8] && (
+          <div className="grid gap-4 grid-rows-4">
+            <div className="h-full max-w-full rounded-lg">
+              <ProjectItem key={itemsToRender[6].id} project={itemsToRender[6]} />
+            </div>
+            <div className="h-full max-w-full rounded-lg">
+              <ProjectItem key={itemsToRender[7].id} project={itemsToRender[7]} />
+            </div>
+            <div className="h-full max-w-full rounded-lg row-span-2">
+              <ProjectItem key={itemsToRender[8].id} project={itemsToRender[8]} />
+            </div>
+          </div>
+        )}
+
+        {itemsToRender[8] && (
+          <div className="grid gap-4 grid-rows-4">
+            <div className="h-full max-w-full rounded-lg">
+              <ProjectItem key={itemsToRender[6].id} project={itemsToRender[6]} />
+            </div>
+            <div className="h-full max-w-full rounded-lg row-span-2">
+              <ProjectItem key={itemsToRender[7].id} project={itemsToRender[7]} />
+            </div>
+            <div className="h-full max-w-full rounded-lg">
+              <ProjectItem key={itemsToRender[8].id} project={itemsToRender[8]} />
+            </div>
+          </div>
+        )}
       </div>
+
     </Card>
   );
 }
