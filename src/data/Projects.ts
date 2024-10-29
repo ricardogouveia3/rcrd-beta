916de66db664d22f4bd7d37cb762baaa1d63a3e3
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2024-10-29 11:46:02 -0300

    change project title

--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -211,7 +211,7 @@ const projects = [
   },
   {
     id: 12,
-    title: "Project 11",
+    title: "Project 12",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     link: "https://github.com",
