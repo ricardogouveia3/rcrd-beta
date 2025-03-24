e6b33b6afb6c9bb18dd235ad48468a1e276becc6
ricardogouveia3 <ricardogouveia3_@hotmail.com>
2025-03-24 08:31:15 -0300

    fix map hover behavior

--- src/components/Location/LocationMap.tsx
+++ src/components/Location/LocationMap.tsx
@@ -13,7 +13,7 @@ export default function LocationMap({ map, info = false, hover = false }: Readon
   const hoverMapClassnames = hover ? `scale-125` : ``;
 
   return (
-    <div className={`w-full h-full ${mapStyleIfInfoLoaded} transition-all`}>
+    <div className={`w-full h-full ${mapStyleIfInfoLoaded} transition-all overflow-hidden`}>
       <a href={map.link} target="_blank" className="relative w-full h-full block">
         <div className={`absolute h-full w-full bg-black opacity-0 dark:opacity-40`} />
         <img src={map.imgSrc} alt={map.alt} className={`transition-all  object-cover object-center h-full w-full max-h-40 lg:max-h-full ${hoverMapClassnames}`}/>
